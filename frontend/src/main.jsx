47e94d3a4ba15974d635de4c4b3dc948cd2d27eb
Abhay-S-R <abhaysr946@gmail.com>
2025-05-29 01:01:31 +0530

    Updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import EcoPlayApp from './App.jsx'
+import { HomePage, EcoPlayApp } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <EcoPlayApp />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<EcoPlayApp />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
